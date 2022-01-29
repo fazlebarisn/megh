11a13b61f20735c91c9d5b4d2ceaa41187a9c0bf
Fazle Bari <fazlebarisn@gmail.com>
2022-01-29 20:40:45 +0600

    plugin head

---
+++ megh.php
@@ -0,0 +1,11 @@
+<?php
+/**
+Plugin Name: Megh
+Plugin URI: https://www.chitabd.com/
+Description: New plugin to learn something new
+Version: 1.0.0
+Author: Fazle Bari
+Author URI: https://www.chitabd.com/
+Licence: GPL Or leater
+Text Domain: megh
+*/
