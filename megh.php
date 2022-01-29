631d5d369e66cfd85821f958e423450d8599e636
Fazle Bari <fazlebarisn@gmail.com>
2022-01-29 21:55:43 +0600

    Add filter for users

--- megh.php
+++ megh.php
@@ -32,8 +32,8 @@ function meghWordCount( $content ){
     $stripped_content = strip_tags( $content );
     $word_number = str_word_count( $stripped_content );
     $lable = __('Total number of word' , 'megh');
-    $lable = apply_filters( 'word_count_lable' , $lable );
-    $tag = apply_filters( 'word_count_tag' , 'h2' );
+    $lable = apply_filters( 'megh_word_count_lable' , $lable );
+    $tag = apply_filters( 'megh_word_count_tag' , 'h2' );
     $content.= sprintf( '<%s>%s: %s</%s>' , $tag, $lable , $word_number, $tag );
 
     return $content;
