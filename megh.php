73d615343dae519ff3acebd4cacf2b075dc083e9
Fazle Bari <fazlebarisn@gmail.com>
2022-01-29 21:19:06 +0600

    display word count

--- megh.php
+++ megh.php
@@ -16,7 +16,25 @@ Domain Path: /languages/
  *
  * @return void
  */
-function megh_load_textdomain(){
+function meghLoadTextdomain(){
     load_plugin_textdomain( 'megh' ,false, dirname(__FILE__ . '/languages') );
 }
-add_action( 'plugins_loaded' , 'megh_load_textdomain');
+add_action( 'plugins_loaded' , 'meghLoadTextdomain');
+
+/**
+ * count the word of post
+ *
+ * @param [type] $content
+ * @return void
+ */
+function meghWordCount( $content ){
+
+    $stripped_content = strip_tags( $content );
+    $word_number = str_word_count( $stripped_content );
+    $lable = __('Total number of word' , 'megh');
+    $content.= sprintf( '<h2>%s: %s</h2>' , $lable , $word_number );
+
+    return $content;
+
+}
+add_filter( 'the_content' , 'meghWordCount');
