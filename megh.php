82c1494186c282952c95d72d642cd9feb8b45f8f
Fazle Bari <fazlebarisn@gmail.com>
2022-06-10 12:28:02 +0600

    enqueue main js file

--- megh.php
+++ megh.php
@@ -20,3 +20,16 @@ function meghLoadTextdomain(){
     load_plugin_textdomain( 'megh' ,false, dirname(__FILE__ . '/languages') );
 }
 add_action( 'plugins_loaded' , 'meghLoadTextdomain');
+
+/**
+ * Enqueue Scripts
+ *
+ * @param [type] $screen
+ * @return void
+ */
+function meghAssets(){
+    
+    wp_enqueue_script( 'megh-main-js', plugin_dir_url(__FILE__).'assets/js/megh-main.js' , array('jquery'), time(), true );
+
+}
+add_action( 'admin_enqueue_scripts', 'meghAssets');
