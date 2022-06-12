cfd9d49459e6830dd7903189efb98396ee01b859
Fazle Bari <fazlebarisn@gmail.com>
2022-06-12 21:26:49 +0600

    add html css and js for faq section

--- megh.php
+++ megh.php
@@ -9,17 +9,17 @@ Author URI: https://www.chitabd.com/
 Licence: GPLv2 Or leater
 Text Domain: megh
 Domain Path: /languages/
-*/
+ */
 
 /**
  * load plugin textdomain
  *
  * @return void
  */
-function meghLoadTextdomain(){
-    load_plugin_textdomain( 'megh' ,false, dirname(__FILE__ . '/languages') );
+function meghLoadTextdomain() {
+    load_plugin_textdomain( 'megh', false, dirname( __FILE__ . '/languages' ) );
 }
-add_action( 'plugins_loaded' , 'meghLoadTextdomain');
+add_action( 'plugins_loaded', 'meghLoadTextdomain' );
 
 /**
  * Enqueue Scripts
@@ -27,30 +27,66 @@ add_action( 'plugins_loaded' , 'meghLoadTextdomain');
  * @param [type] $screen
  * @return void
  */
-function meghAssets(){
-    
-    wp_enqueue_script( 'megh-main-js', plugin_dir_url(__FILE__).'assets/js/megh-main.js' , array('jquery'), time(), true );
-    wp_enqueue_style( 'megh-main-js', plugin_dir_url(__FILE__).'assets/css/megh-main.css' , array(), '1.1', 'all' );
+function meghAssets() {
+
+    wp_enqueue_script( 'megh-main-js', plugin_dir_url( __FILE__ ) . 'assets/js/megh-main.js', [ 'jquery' ], time(), true );
+    wp_enqueue_style( 'megh-main-js', plugin_dir_url( __FILE__ ) . 'assets/css/megh-main.css', [], '1.1', 'all' );
 
 }
-add_action( 'wp_enqueue_scripts', 'meghAssets');
+add_action( 'wp_enqueue_scripts', 'meghAssets' );
 
 //include_once plugin_dir_url(__FILE__) . 'includes/functions.php';
 
-function woocommerce_before_single_product_callback(){
-    if( !is_product()){
+function woocommerce_before_single_product_callback() {
+    if ( !is_product() ) {
         return;
     }
     echo "<div class='megh'>";
-    
-}
-add_action( 'woocommerce_before_single_product' , 'woocommerce_before_single_product_callback');
 
-function woocommerce_after_single_product_summary_callback(){
-    if( !is_product()){
+}
+//add_action( 'woocommerce_before_single_product' , 'woocommerce_before_single_product_callback');
+
+function woocommerce_after_single_product_callback() {
+    if ( !is_product() ) {
         return;
     }
-    echo "</div>";
-    
+    ?>
+    <div class="container">
+  <h2>Frequently Asked Questions</h2>
+  <div class="accordion">
+    <div class="accordion-item">
+      <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Why is the moon sometimes out during the day?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Why is the sky blue?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Will we ever discover aliens?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">How much does the Earth weigh?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-5" aria-expanded="false"><span class="accordion-title">How do airplanes stay up?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    <?php
+
 }
-add_action( 'woocommerce_after_single_product_summary' , 'woocommerce_after_single_product_summary_callback');
+add_action( 'woocommerce_after_single_product', 'woocommerce_after_single_product_callback' );
