345c77059e8fae481246fd883fbbd1e041cf6f34
Fazle Bari <fazlebarisn@gmail.com>
2022-06-14 22:36:58 +0600

    display data in font page

--- megh.php
+++ megh.php
@@ -119,58 +119,37 @@ function sfaq_save_field_data( $post_id ){
   // Updating here 
   update_post_meta( $post_id,'faq_1', esc_attr( $_faq_1 ) ); 
   update_post_meta( $post_id,'faq_ans_1', esc_attr( $_faq_ans_1 ) ); 
-  
+
 }
 add_action( 'woocommerce_process_product_meta', 'sfaq_save_field_data' );
 
 
-
-
-
-
+// show data in product page in bottom
 
 function woocommerce_after_single_product_callback() {
   
     if ( !is_product() ) {
         return;
     }
+    $product_id = get_the_ID();
+    $faq_1 = get_post_meta($product_id,'faq_1', true);
+    $_faq_ans_1 = get_post_meta($product_id,'faq_ans_1', true);
     //the_field('questions_1');
-    //var_dump(the_field('questions'));
-    if( !function_exists('get_field') || null == get_field('questions') ){
-      return;
-    }else{
-      $questions = get_field('questions');
-    }
+    //var_dump($faq_1);
 
-    if( !empty($questions) ){
-      $question_count = count($questions) / 2;
-      //var_dump($questions);
-      ?>
-        <div class="container">
-          <h2>Frequently Asked Questions</h2>
-          <div class="accordion">
-          <?php 
-            for( $i=1; $i<=$question_count; $i++ ){
-              $qs = 'qs_' . $i;
-              $ans = 'ans_' .$i;
-              if( !empty($questions[$qs] ) ){
-            ?>
-              <div class="accordion-item">
-                <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title"><?php echo $questions[$qs] ?? $questions[$qs]; ?></span><span class="icon" aria-hidden="true"></span></button>
-                <div class="accordion-content">
-                  <p><?php echo $questions[$ans] ?? $questions[$ans]; ?></p>
-                </div>
-              </div>
-              <?php
-               }else{
-                echo "<p>No question!</p>";
-               }
-              }
-            ?>
+?>
+  <div class="container">
+    <h2>Frequently Asked Questions</h2>
+    <div class="accordion">
+        <div class="accordion-item">
+          <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title"><?php echo $faq_1 ?? $faq_1; ?></span><span class="icon" aria-hidden="true"></span></button>
+          <div class="accordion-content">
+            <p><?php echo $_faq_ans_1 ?? $_faq_ans_1; ?></p>
           </div>
         </div>
-      <?php
-    }
-  
+    </div>
+  </div>
+  <?php
+    
 }
 add_action( 'woocommerce_after_single_product', 'woocommerce_after_single_product_callback' );
